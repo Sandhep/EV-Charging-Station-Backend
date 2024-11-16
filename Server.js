3b657a2d1b592c47886046c16a47c53a05811f48
Sandhep <b.sandhep111@gmail.com>
2024-11-17 00:09:40 +0530

    added cors

--- Server.js
+++ Server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';  // Import cors
 import User from './UtilityClasses/User.js';
 import authenticateToken from './middleware/authenticateToken.js';
 
@@ -8,11 +9,22 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());  // Enable CORS for all routes
 app.use(express.json());
+
+const corsOptions = {
+  origin: 'http://localhost:3000',  // Only allow requests from this origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'],  // Allowed headers
+};
+
+app.use(cors(corsOptions));  // Apply CORS options
 
 // Register a new user
 app.post('/api/register', async (req, res) => {
   const { name, email, phoneNumber, password } = req.body;
+
+  console.log(req.body);
 
   try {
     const newUser = await User.register({ name, email, phoneNumber, password });
@@ -38,6 +50,7 @@ app.post('/api/login', async (req, res) => {
 app.get('/api/user', authenticateToken, async (req, res) => {
   try {
     const user = await User.getProfile(req.user.userID);
+    console.log(user);
     res.status(200).json(user);
   } catch (error) {
     res.status(404).json({ message: error.message });
