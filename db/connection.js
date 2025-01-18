7621be71bab1d464192957e8d6f6646992d3c050
Sandhep <b.sandhep111@gmail.com>
2025-01-19 00:19:25 +0530

    db connection update

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config(); // Load environment variables from .env
 
-import pkg from 'pg';
-const { Client } = pkg;
+import pg from 'pg';
+const { Client } = pg;
 
 // Database connection setup
 const client = new Client({
@@ -11,11 +11,27 @@ const client = new Client({
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+    keepAlive: true,
 });
 
-// Connect to the database
-client.connect().catch(err => {
-    console.error('Failed to connect to the database:', err.stack);
-});
+client.connect()
+     .then(()=>{
+        console.log("DB Connected successfully");
+     })
+     .catch(err=>{
+        console.error("Failed to connect to the Database",err.stack);
+     })
+
+client.on('error', async (err) => {
+
+    console.error('Database connection error:', err.stack);
+    console.log('Attempting to reconnect...');
+    try {
+        await client.connect();
+        console.log('Reconnected to the database.');
+        } catch (reconnectErr) {
+        console.error('Failed to reconnect:', reconnectErr.stack);
+        }
+    });
 
 export default client;
