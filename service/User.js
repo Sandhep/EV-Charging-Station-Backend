a6eeb482f780d4eb5c0d82db1293317f8f559189
Sandhep <b.sandhep111@gmail.com>
2025-01-21 01:03:34 +0530

    connected user service with db

--- service/User.js
+++ service/User.js
@@ -1,117 +1,83 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-//import db from '../db/connection.js'; // PostgreSQL client
 import StationFinder from './userservice/StationFinder.js';
 import ReservationSystems from './userservice/ReservationSystems.js';
 import RealTimeUpdates from './userservice/RealTimeUpdates.js';
-import UserModel from '../repository/models/User.js';
+import db from '../repository/models/index.js';
 
 class User {
 
-  // Register a new user
   async register({ name, email, phoneNumber, password }) {
+
     if (!name || !email || !phoneNumber || !password) {
       throw new Error('All fields are required');
     }
 
-    // Check if the user already exists
-    //const userExistsQuery = 'SELECT * FROM users WHERE "Email" = $1';
-    //const userExists = await db.query(userExistsQuery, [email]);
-
-    const userExists = await UserModel.findOne({
+    const userExists = await db.UserModel.findOne({
       where: {
         Email: email,
       },
     });
     
-
-    if (userExists.rows.length > 0) {
+    if (userExists) {
       throw new Error('User already exists');
     }
 
-    // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Insert the new user into the database
-   /* const insertUserQuery = `
-      INSERT INTO users ("Name", "Email", "PhoneNumber", "PasswordHash", "Role", "LoyaltyPoints")
-      VALUES ($1, $2, $3, $4, 'user', 0)
-      RETURNING *;
-    `;
-
-      const newUser = await db.query(insertUserQuery, [
-      name,
-      email,
-      phoneNumber,
-      hashedPassword,
-    ]);
-   */
-    const newUser = await UserModel.create({
+    const newUser = await db.UserModel.create({
       Name: name,
       Email: email,
       PhoneNumber: phoneNumber,
       PasswordHash:hashedPassword
     });
     
-    return newUser.rows[0]; // Return the newly created user object
+    return newUser; 
   }
 
-  // Login a user
   async login({ email, password }) {
+
     if (!email || !password) {
       throw new Error('Email and password are required');
     }
-
-    // Find the user by email
-   // const findUserQuery = 'SELECT * FROM users WHERE "Email" = $1';
-    //const user = await db.query(findUserQuery, [email]);
     
-    const user = await UserModel.findOne({
+    const user = await db.UserModel.findOne({
       where: {
         Email: email,
       },
     });
     
-    if (user.rows.length === 0) {
+    if (!user) {
       throw new Error('Invalid credentials');
     }
 
-    const foundUser = user.rows[0];
-
-    // Check if the password is correct
-    const isPasswordValid = await bcrypt.compare(password, foundUser.PasswordHash);
+    const isPasswordValid = await bcrypt.compare(password, user.PasswordHash);
 
     if (!isPasswordValid) {
       throw new Error('Invalid credentials');
     }
 
-    // Generate a JWT token
     const token = jwt.sign(
-      { userID: foundUser.UserID, email: foundUser.Email },
+      { userID: user.UserID, email: user.Email },
       process.env.JWT_SECRET || 'your_secret_key'
     );
 
     return { message: 'Login successful', token };
   }
 
-  // Fetch a user's profile (protected by JWT)
   async getProfile(userID) {
 
-    //const getUserQuery = 'SELECT * FROM users WHERE "UserID" = $1';
-    //const user = await db.query(getUserQuery, [userID]);
-
-    const user = await UserModel.findOne({
+    const user = await db.UserModel.findOne({
       where: {
         UserID: userID,
       },
     });
     
-
-    if (user.rows.length === 0) {
+    if (!user) {
       throw new Error('User not found');
     }
 
-    return user.rows[0];
+    return user;
   }
 
   async getStationdata(location,type){
