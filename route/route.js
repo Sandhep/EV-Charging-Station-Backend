655fd7a93871cf40335099bb1d71533a862437c8
Sandhep <b.sandhep111@gmail.com>
2025-01-21 01:14:19 +0530

    removed test route

--- route/route.js
+++ route/route.js
@@ -4,7 +4,6 @@ import authenticateToken from '../middleware/authenticateToken.js';
 
 const router = express.Router();
 
-router.get('/test', UserController.gettest);
 router.get('/user',authenticateToken, UserController.getUser);
 router.post('/login',UserController.userLogin);
 router.post('/register',UserController.userRegister);
